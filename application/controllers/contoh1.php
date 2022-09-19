75c9616c9fb09361da84293fcc3cb122179a93f5
surtiayu <surtiayu16@gmail.com>
2022-09-19 15:27:52 +0700

    penambahan isi application/controllers/contoh1.php

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php 
+class Contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>Perkenalkan</h>";
+		echo "Nama saya Surti Ayu
+		Saya tinggal di Jungkat
+		Makanan favorit saya adalah
+		sate dan bakso";
+	}
+}
